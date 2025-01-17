97d11a58f3dc621c1915e961be97ce5279445fb7
Dmitro Sysoev <dimasysoev20000@gmail.com>
2025-01-17 17:12:45 +0200

    Sort system

--- comments/views.py
+++ comments/views.py
@@ -7,11 +7,23 @@ from .serializers import CommentSerializer
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.filter(parent=None)
     serializer_class = CommentSerializer
-    permission_classes = [AllowAny]  # Разрешаем доступ как залогиненным, так и анонимным пользователям
+    paginate_by = 25
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        # Получаем параметры сортировки из запроса
+        sort_by = self.request.query_params.get('sort_by', 'created_at')
+        order = self.request.query_params.get('order', 'asc')
+
+        if order == 'desc':
+            sort_by = f'-{sort_by}'
+
+        # Возвращаем отсортированные комментарии
+        return Comment.objects.filter(parent=None).order_by(sort_by)
 
     def perform_create(self, serializer):
         user = self.request.user
         if user.is_authenticated:
-            serializer.save(user=user)  # Передаём текущего пользователя
+            serializer.save(user=user)
         else:
-            serializer.save()  # Сохраняем для анонимных
+            serializer.save()
