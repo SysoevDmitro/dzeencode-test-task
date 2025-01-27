3046e055f0514d665206c349c3e3e9c15575abc7
SysoevDmitro <113758312+SysoevDmitro@users.noreply.github.com>
2025-01-27 16:50:46 +0200

    Update settings.py

--- spa/settings.py
+++ spa/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'spa.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "NAME": "server",
+        "USER": "user",
+        "PASSWORD": "password",
     }
 }
 
