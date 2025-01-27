f9421f9c4b97866b17242b622ac345f15ce6b356
Dmitro Sysoev <dimasysoev20000@gmail.com>
2025-01-28 01:59:09 +0200

    s s

--- spa/settings.py
+++ spa/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "12")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'dzeencode-test-task-production.up.railway.app']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 API_URL = 'http://localhost:8000'
 
